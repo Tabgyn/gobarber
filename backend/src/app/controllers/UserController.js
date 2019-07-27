b4af399c4a52682874cb815af9e4177be2cf9b45
Tiago Azevedo Borges <tabgyn@gmail.com>
2019-07-27 15:42:04 -0300

    add user update method

--- backend/src/app/controllers/UserController.js
+++ backend/src/app/controllers/UserController.js
@@ -14,7 +14,25 @@ class UserController {
   }
 
   async update(req, res) {
-    return res.json({ ok: true });
+    const { email, currentPassword } = req.body;
+
+    const user = await User.findByPk(req.userId);
+
+    if (email !== user.email) {
+      const userExists = await User.findOne({ where: { email } });
+
+      if (userExists) {
+        return res.status(400).json({ error: 'Email already used' });
+      }
+    }
+
+    if (!!currentPassword && !(await user.checkPassword(currentPassword))) {
+      return res.status(401).json({ error: 'Current password does not match' });
+    }
+
+    const { id, name, provider } = await user.update(req.body);
+
+    return res.json({ id, name, email, provider });
   }
 }
 
