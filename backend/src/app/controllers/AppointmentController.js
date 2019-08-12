c7b74dfafa463e65bcdff2d2b71a5e647ff30282
Tiago Azevedo Borges <tabgyn@gmail.com>
2019-08-12 18:07:02 -0300

    add notifications

--- backend/src/app/controllers/AppointmentController.js
+++ backend/src/app/controllers/AppointmentController.js
@@ -1,9 +1,12 @@
 import * as Yup from 'yup';
-import { startOfHour, parseISO, isBefore } from 'date-fns';
+import { startOfHour, parseISO, isBefore, format } from 'date-fns';
+import pt from 'date-fns/locale/pt';
 
 import Appointment from '../models/Appointment';
 import User from '../models/User';
 import File from '../models/File';
+
+import Notification from '../schemas/notification';
 
 class AppointmentController {
   async index(req, res) {
@@ -45,6 +48,15 @@ class AppointmentController {
     }
 
     const { provider_id, date } = req.body;
+
+    /**
+     * Check if user is not the provider
+     */
+    if (req.userId === provider_id) {
+      return res
+        .status(401)
+        .json({ error: 'A provider cannot create appointments for yourself' });
+    }
 
     /**
      * Check if provider_id is a provider
@@ -89,6 +101,18 @@ class AppointmentController {
       user_id: req.userId,
       provider_id,
       date,
+    });
+
+    /**
+     * Notify appointment provider
+     */
+    const user = await User.findByPk(req.userId);
+    const formatedDate = format(hourStart, "'dia' dd 'de' MMMM', às' H:mm'h'", {
+      locale: pt,
+    });
+    await Notification.create({
+      content: `Novo agendamento de ${user.name} para ${formatedDate}`,
+      user: provider_id,
     });
 
     return res.json(appointment);
