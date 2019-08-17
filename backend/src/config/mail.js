07619f461dcc89b76ea873c6a3e2c0b00106fcc1
Tiago Azevedo Borges <tabgyn@gmail.com>
2019-08-17 15:30:46 -0300

    add queues

---
+++ backend/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  auth: {
+    user: 'c0e85fce0cdec3',
+    pass: 'bfad0f2fb8cc1e',
+  },
+  secure: false,
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
