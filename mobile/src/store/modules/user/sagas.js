f87c62484b4fb8347dcc7f5ea6c311860376ce34
Tiago Azevedo Borges <tabgyn@gmail.com>
2019-10-05 16:11:15 -0300

    add profile view

--- mobile/src/store/modules/user/sagas.js
+++ mobile/src/store/modules/user/sagas.js
@@ -7,18 +7,17 @@ import api from '~/services/api';
 
 export function* updateProfile({ payload }) {
   try {
-    const { name, email, avatar_id, ...rest } = payload.data;
+    const { name, email, ...rest } = payload.data;
 
     const profile = {
       name,
       email,
-      avatar_id,
       ...(rest.currentPassword ? rest : {}),
     };
 
     const response = yield call(api.put, 'users', profile);
 
-    Alert.alert('Falha ao atualizar o perfil', 'Perfil atualizado com sucesso');
+    Alert.alert('Sucesso', 'Perfil atualizado com sucesso');
 
     yield put(updateProfileSuccess(response.data));
   } catch (error) {
