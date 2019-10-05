8b0507fa3c697956c2a4c58ae3d0d4b085fa8b79
Tiago Azevedo Borges <tabgyn@gmail.com>
2019-10-05 15:17:54 -0300

    add styles to routes

--- mobile/src/pages/Dashboard/index.js
+++ mobile/src/pages/Dashboard/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
-import { View } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-// import { Container } from './styles';
+import Background from '~/components/Background';
+
+import { Container, Title, List } from './styles';
 
 export default function Dashboard() {
-  return <View />;
+  return (
+    <Background>
+      <Container />
+    </Background>
+  );
 }
+
+Dashboard.navigationOptions = {
+  tabBarLabel: 'Agendamentos',
+  tabBarIcon: ({ tintColor }) => (
+    <Icon name="event" size={20} color={tintColor} />
+  ),
+};
---
+++ mobile/src/pages/Profile/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import Background from '~/components/Background';
+
+import {
+  Container,
+  Title,
+  Separator,
+  Form,
+  FormInput,
+  SubmitButton,
+  LogoutButton,
+} from './styles';
+
+export default function Profile() {
+  return (
+    <Background>
+      <Container />
+    </Background>
+  );
+}
+
+Profile.navigationOptions = {
+  tabBarLabel: 'Meu perfil',
+  tabBarIcon: ({ tintColor }) => (
+    <Icon name="person" size={20} color={tintColor} />
+  ),
+};
